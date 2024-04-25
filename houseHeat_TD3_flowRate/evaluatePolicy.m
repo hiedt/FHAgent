02e2d2f2f893cc9f3ec379065c23dfe4979a33ff
hieudtrung <nigel_xzzz@yahoo.com.vn>
2024-04-25 10:48:03 +0200

    - old MATLAB codegen evalutePolicy

--- houseHeat_TD3_flowRate/evaluatePolicy.m
+++ houseHeat_TD3_flowRate/evaluatePolicy.m
@@ -2,11 +2,11 @@ function action1 = evaluatePolicy(observation1)
 %#codegen
 
 % Reinforcement Learning Toolbox
-% Generated on: 17-Apr-2024 15:53:52
+% Generated on: 24-Apr-2024 22:58:38
 
 persistent policy;
 if isempty(policy)
-	policy = coder.loadRLPolicy("agentData3.mat");
+	policy = coder.loadRLPolicy("agentData.mat");
 end
 % evaluate the policy
 action1 = getAction(policy,observation1);
