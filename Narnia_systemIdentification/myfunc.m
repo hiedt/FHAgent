c376e659d14a0bfd307c6cff96db76ed28ede1df
hieudtrung <nigel_xzzz@yahoo.com.vn>
2024-05-08 14:08:17 +0200

    + chamber SI with non-linear ARX

--- Narnia_systemIdentification/myfunc.m
+++ Narnia_systemIdentification/myfunc.m
@@ -1,9 +1,9 @@
-function [A,B,C,D,K] = myfunc(par,Ts)
+function [A,B,C,D,K,x0] = myfunc(par,Ts)
     A = [0 0 1; -par(1)*par(2) 0 -par(2)*par(3); 0 0 0];
     B = [0; par(2); 0];
-    C = [1 1 0];
+    C = [1 0 0];
     D = zeros(1,1);
     K = [0;  par(1)*par(2); 0];
-    % x0 = [0; 0; 0]; % A guess of initial state
+    x0 = [0; 0; 0]; % A guess of initial state
 end
 
