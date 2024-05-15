daf5c2049fef7d6b05810369d1b6a2ac860a9960
hieudtrung <nigel_xzzz@yahoo.com.vn>
2024-05-15 10:56:19 +0200

    * LQR reward + dTout obs, succeed

--- Narnia_TD3_energy/QuadraticGains.m
+++ Narnia_TD3_energy/QuadraticGains.m
@@ -1,9 +1,9 @@
 classdef QuadraticGains
     % Tunable gains for Linear Quadratic reward (r2)
     properties (Constant)
-        Q = [2e-2 0; 0 1e-2];
-        R = 1e-3;
-        N = [0; 0];
+        Q = [1/40 0; 0 1e-2];
+        R = 5e-3;
+        N = [4e-3; 5e-3];
     end
 end
 
