46b6877920768625d02c8b4bbc639f2982d0dc5c
hieudtrung <nigel_xzzz@yahoo.com.vn>
2024-05-17 23:58:42 +0200

    + 6obs with random load, only error cost, no action cost

---
+++ Narnia_TD3_pwmDC/EdgeCase6ObsVarLoad.m
@@ -0,0 +1,13 @@
+classdef EdgeCase6ObsVarLoad
+    %EDGECASE_6OBSVARLOAD Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Constant)
+        seed = 1;
+        To = -9.1946;
+        r1 = 17.4039;
+        r2 = 24.7247;
+        T0 = 12.7246;
+    end
+end
+
